5f51aa004dae1f7cf0efa52f53992076cc2fd4d3
RuanPSilva <gabrielvicentesantos@outlook.com>
2025-03-19 20:32:39 -0300

    Criando a estruturas de classes do projeto

---
+++ Projeto2B/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto2B
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
